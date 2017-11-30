2912f4175d149feb0ff0568923d84bed0c0894ac
Simonas Mikulis <simonas.mikulis@devbridge.com>
2017-11-30 12:52:57 +0200

    added json server ajax calls

--- lab4/js/index.js
+++ lab4/js/index.js
@@ -1,59 +1,125 @@
+"use strict";
 $(function () {
-  const checkboxSelector = '.todo input[type="checkbox"]';
-  const formEl = $('form');
-  const titleEl = $('#todo-title');
-  const pointsEl = $('#todo-points');
-  const dateEl = $('#todo-date');
-  
-  function bindCheckboxes() {
-    $(checkboxSelector).on('click.checkboxes', e => {
-      label = $(event.target).parent().find('label');
-      label.toggleClass('removed');
-    })
-  }
-  
-  function rebindCheckboxes() {
-    $(checkboxSelector).off('click.checkboxes');
-    bindCheckboxes();
-  }
-
-  function bindFormSubmit() {
-    formEl.on('submit', e => {
-      e.preventDefault();
-      if (!validateTitle() || !validatePoints() || !validateDate()) {
-        return;
-        // show popup or something
-      }
-      const element = $('<div class="todo">' +
-                          '<input type="checkbox"/>' +
-                          `<label for="checkbox">${titleEl[0].value}</label>` +
-                        '</div>');
-      titleEl.value = '';
-      $('.todo-list').append(element);
-      rebindCheckboxes();
-    })
-  }
-
-  function validateTitle() {
-    if (titleEl[0].value === '')
-      return false;
-  }
-
-  function validatePoints() {
-    const points = parseInt(pointsEl[0].value);
-    if (!Number.isInteger(points) || points < 0)
-      return false;
-  }
-  
-  function validateDate() {
-    if (!/^\d{4}-\d{2}-\d{2}$/.test(dateEl[0].value))
-      return false;
-    const date = new Date(dateEl[0].value);
-    if (date.getDate() != dateEl[0].value.substring(8, 10)) {
-      return false;
+    const todoDatabaseUrl = 'http://localhost:3000/tasks';
+    const checkboxSelector = '.todo input[type="checkbox"]';
+    const formEl = $('form');
+    const titleEl = $('#todo-task');
+    const priorityEl = $('#todo-priority');
+    const dateEl = $('#todo-date');
+    const todoListEl = $('.todo-list');
+    let todos = [];
+    function bindCheckboxes() {
+        $(checkboxSelector).on('click.checkbox', event => {
+            const row = $(event.target).closest('tr');
+            const todo = $.grep(todos, todo => todo.id == Number(row.attr('todoid')))[0];
+            todo.isDone = !todo.isDone;
+            updateTodo(todo);
+            row.toggleClass('strikeout');
+        });
     }
-  }
-  
-  bindCheckboxes();
-  bindFormSubmit();
-})
+    function rebindCheckboxes() {
+        $(checkboxSelector).off('click.checkbox');
+        bindCheckboxes();
+    }
+    function handleInvalidInput() {
+        return;
+    }
+    function bindFormSubmit() {
+        formEl.on('submit', e => {
+            e.preventDefault();
+            // if (!isTitleValid() || !isPriorityValid() || !isDateValid()) {
+            //   handleInvalidInput()
+            //   alert('values are invalid')
+            //   return
+            //   // show popup or something
+            // }
+            saveNewTodo(createTodoItem(Number(priorityEl.val()), String(titleEl.val()), String(dateEl.val())));
+            clearInputFields();
+            getTodos();
+        });
+    }
+    function isTitleValid() {
+        return titleEl.val() !== '';
+    }
+    function isPriorityValid() {
+        const points = parseInt(String(priorityEl.val()));
+        if (points < 0)
+            return false;
+    }
+    function isDateValid() {
+        if (!/^\d{4}(-|\\|\.)\d{2}(-|\\|\.)\d{2}$/.test(String(dateEl.val())))
+            return false;
+        const date = new Date(String(dateEl.val()));
+        if (date.getDate() != Number(String(dateEl.val()).substring(8, 10))) {
+            return false;
+        }
+    }
+    function clearInputFields() {
+        priorityEl.val(''),
+            titleEl.val(''),
+            dateEl.val('');
+    }
+    function createTodoItem(priority, task, date) {
+        const maxCurrentId = Math.max(...todos.map(todo => todo.id));
+        return {
+            id: maxCurrentId + 1,
+            isDone: false,
+            priority,
+            task,
+            date
+        };
+    }
+    function createTodoItemEl(todo) {
+        return $(`<tr todoid="${todo.id}" class="todo ${todo.isDone ? 'strikeout' : ''}">` +
+            `<td><input type="checkbox" ${todo.isDone ? 'checked' : ''}/></td>` +
+            `<td>${todo.priority}</td>` +
+            `<td>${todo.task}</td>` +
+            `<td>${todo.date}</td>` +
+            '</tr>');
+    }
+    function renderTodos() {
+        todoListEl.find('tbody').html('');
+        todos.forEach(todo => {
+            appendTodo(todo);
+        });
+        bindCheckboxes();
+    }
+    function appendTodo(todo) {
+        todoListEl.find('tbody').append(createTodoItemEl(todo));
+    }
+    function updateTodo(todo) {
+        $.ajax({
+            type: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            url: `${todoDatabaseUrl}/${todo.id}`,
+            data: JSON.stringify(todo),
+            success: () => {
+                console.log('update successful!');
+            }
+        });
+    }
+    function saveNewTodo(todo) {
+        $.ajax({
+            type: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            url: todoDatabaseUrl,
+            data: JSON.stringify(todo),
+            success: () => {
+                console.log('post successful');
+            }
+        });
+    }
+    function getTodos() {
+        todos = [];
+        $.getJSON(todoDatabaseUrl, (tasks) => {
+            todos = tasks;
+            renderTodos();
+        });
+    }
+    getTodos();
+    bindFormSubmit();
+});
---
+++ lab4/js/index.ts
@@ -0,0 +1,157 @@
+$(function () {
+  const todoDatabaseUrl = 'http://localhost:3000/tasks'
+
+  const checkboxSelector = '.todo input[type="checkbox"]'
+  const formEl = $('form')
+  const titleEl = $('#todo-task')
+  const priorityEl = $('#todo-priority')
+  const dateEl = $('#todo-date')
+  const todoListEl = $('.todo-list')
+
+  let todos: Task[] = []
+  
+  function bindCheckboxes() {
+    $(checkboxSelector).on('click.checkbox', event => {
+      const row = $(event.target).closest('tr')
+      const todo: Task = $.grep(todos, todo => todo.id == Number(row.attr('todoid')))[0]
+      todo.isDone = !todo.isDone
+      updateTodo(todo)
+      row.toggleClass('strikeout')
+    })
+  }
+  
+  function rebindCheckboxes() {
+    $(checkboxSelector).off('click.checkbox')
+    bindCheckboxes()
+  }
+
+  function handleInvalidInput() {
+    return
+  }
+
+  function bindFormSubmit() {
+    formEl.on('submit', e => {
+      e.preventDefault()
+      // if (!isTitleValid() || !isPriorityValid() || !isDateValid()) {
+      //   handleInvalidInput()
+      //   alert('values are invalid')
+      //   return
+      //   // show popup or something
+      // }
+
+      saveNewTodo(
+        createTodoItem(
+          Number(priorityEl.val()),
+          String(titleEl.val()),
+          String(dateEl.val()))
+      )
+
+      clearInputFields()
+      getTodos()
+    })
+  }
+
+  function isTitleValid() {
+    return titleEl.val() !== ''
+  }
+
+  function isPriorityValid() {
+    const points = parseInt(String(priorityEl.val()))
+    if (points < 0)
+      return false
+  }
+  
+  function isDateValid() {
+    if (!/^\d{4}(-|\\|\.)\d{2}(-|\\|\.)\d{2}$/.test(String(dateEl.val())))
+      return false
+    const date = new Date(String(dateEl.val()))
+    if (date.getDate() != Number(String(dateEl.val()).substring(8, 10))) {
+      return false
+    }
+  }
+
+  function clearInputFields() {
+    priorityEl.val(''),
+    titleEl.val(''),
+    dateEl.val('')
+  }
+
+  function createTodoItem(priority: number, task: string, date: string): Task {
+    const maxCurrentId = Math.max(...todos.map(todo => todo.id))
+    return {
+      id: maxCurrentId + 1,
+      isDone: false,
+      priority,
+      task,
+      date
+    }
+  }
+
+  function createTodoItemEl(todo: Task) {
+    return $(`<tr todoid="${todo.id}" class="todo ${todo.isDone ? 'strikeout' : ''}">` +
+               `<td><input type="checkbox" ${todo.isDone ? 'checked' : ''}/></td>` +
+               `<td>${todo.priority}</td>` +
+               `<td>${todo.task}</td>` +
+               `<td>${todo.date}</td>` +
+             '</tr>')
+  }
+
+  interface Task {
+    id: number,
+    isDone: boolean,
+    priority: number,
+    task: string,
+    date: string
+  }
+
+  function renderTodos() {
+    todoListEl.find('tbody').html('')
+    todos.forEach(todo => {
+      appendTodo(todo)
+    })
+    bindCheckboxes()
+  }
+
+  function appendTodo(todo: Task) {
+    todoListEl.find('tbody').append(createTodoItemEl(todo))
+  }
+
+  function updateTodo(todo: Task) {
+    $.ajax({
+      type: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      url: `${todoDatabaseUrl}/${todo.id}`,
+      data: JSON.stringify(todo),
+      success: () => {
+        console.log('update successful!')
+      }
+    })
+  }
+
+  function saveNewTodo(todo: Task) {
+    $.ajax({
+      type: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      url: todoDatabaseUrl,
+      data: JSON.stringify(todo),
+      success: () => {
+        console.log('post successful')
+      }
+    })
+  }
+
+  function getTodos() {
+    todos = []
+    $.getJSON(todoDatabaseUrl, (tasks: Task[]) => {
+      todos = tasks
+      renderTodos()
+    })
+  }
+  
+  getTodos()
+  bindFormSubmit()
+})
